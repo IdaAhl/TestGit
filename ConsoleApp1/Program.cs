5782602d9edc7191b0a227b762a18cb943166bd4
Ida Ahlström <ida.ahlstrom@collectorbank.se>
2018-03-19 13:49:07 +0100

    testar

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp1
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hej");
             Console.WriteLine("Hej");
-            Console.WriteLine("Hej igen!");
+         
             Console.WriteLine("Sparar i GitHub");
             
         }
