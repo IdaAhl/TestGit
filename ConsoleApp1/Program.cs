96c18df638e43dde44931a1c2f43b76d2eb20604
IdaAhl <37538128+IdaAhl@users.noreply.github.com>
2018-03-19 13:47:31 +0100

    Update Program.cs
    spara igen.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApp1
             Console.WriteLine("Hej");
             Console.WriteLine("Hej");
             Console.WriteLine("Hej igen!");
+            Console.WriteLine("Sparar i GitHub");
+            
         }
     }
 }
